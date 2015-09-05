c4e167da1db7efc997c3331cf756271e3fb5b089
moses.mansaray <m_mans@hotmail.com>
2015-09-05 01:39:11 +0100

    [karate-chop] adds module for karate-chop with test skeleton

---
+++ karate-chop/src/test/java/com/mosesmansaray/kata/PositionFinderTest.java
@@ -0,0 +1,31 @@
+package com.mosesmansaray.kata;
+
+/**
+ * Created on 05/09/2015.
+ *
+ * @author moses.mansaray
+ */
+public class PositionFinderTest {
+
+
+  public void shouldReturnPositionAsInt() {
+
+  }
+
+  public void shouldReturnNegativeIntForEmptyArray() {
+
+  }
+
+  public void shouldReturnPostivePositionIfValueExistInArray() {
+
+  }
+
+  public void shouldReturnNegativePositionIfValueDoesNotExistInArray() {
+
+  }
+
+  public void shouldReturnPositionAsExpected() {
+
+  }
+
+}
