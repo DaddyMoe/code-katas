e53ef0ff6ef026ced4ca0a2b5e3c7715edf5c31d
moses.mansaray <m_mans@hotmail.com>
2015-09-05 05:16:15 +0100

    [karate-chop] adds implementation for  karate-chop logic by recursive strategy

--- karate-chop/src/main/java/com/mosesmansaray/kata/PositionFinder.java
+++ karate-chop/src/main/java/com/mosesmansaray/kata/PositionFinder.java
@@ -1,9 +1,52 @@
 package com.mosesmansaray.kata;
 
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Created on 05/09/2015.
  *
  * @author moses.mansaray
  */
 public class PositionFinder {
+  private final static Logger LOGGER = Logger.getLogger(PositionFinder.class.getName());
+
+  public int getPosition(int[] values, int value) {
+    if (values == null || values.length < 1) {
+      return -1;
+    }
+    return getPosition(values, 0, getHigh(values), value);
+  }
+
+  private int getPosition(int[] valuesArray, int low, int high, int value) {
+    LOGGER.log(Level.INFO, "Get Position of " + value + ". From " + Arrays.toString(valuesArray));
+    LOGGER.log(Level.INFO, "Attempting with : low:" + low + ", high:" + high);
+
+    if (high < low) {
+      return -1;
+    }
+
+    int midPoint = getMidPoint(low, high);
+
+    if (valuesArray[midPoint] > value) {
+      int highModified = midPoint - 1;
+      return getPosition(valuesArray, low, highModified, value);
+    } else if (valuesArray[midPoint] < value) {
+      int lowModified = midPoint + 1;
+      return getPosition(valuesArray, lowModified, high, value);
+    } else {
+      LOGGER.log(Level.INFO, "Got Position of " + value + ". Found = " + midPoint);
+      return midPoint;
+    }
+  }
+
+  public int getMidPoint(int low, int high) {
+    return (low + high) / 2;
+  }
+
+  public int getHigh(int[] valueArray) {
+    return valueArray.length - 1;
+  }
+
 }
--- karate-chop/src/test/java/com/mosesmansaray/kata/PositionFinderTest.java
+++ karate-chop/src/test/java/com/mosesmansaray/kata/PositionFinderTest.java
@@ -1,31 +1,95 @@
 package com.mosesmansaray.kata;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created on 05/09/2015.
  *
  * @author moses.mansaray
  */
 public class PositionFinderTest {
+  private final PositionFinder positionFinder = new PositionFinder();
 
+  @Test
+  public void shouldReturnHighGivenArray() {
+    String errorMsg = "midpoint not as expected";
 
-  public void shouldReturnPositionAsInt() {
-
+    int[] valueArray = {1, 2, 3, 4, 5};
+    int high = positionFinder.getHigh(valueArray);
+    assertEquals(errorMsg, 4, high);
   }
 
+  @Test
+  public void shouldReturnMidPointOfGivenTwoPoint() {
+    assertMidPointIsAsExpected(0, 3, 1);
+    assertMidPointIsAsExpected(0, 4, 2);
+    assertMidPointIsAsExpected(0, 5, 2);
+    assertMidPointIsAsExpected(0, 6, 3);
+    assertMidPointIsAsExpected(3, 6, 4);
+    assertMidPointIsAsExpected(0, 7, 3);
+  }
+
+  private void assertMidPointIsAsExpected(int low, int high, int expectedMidPoint) {
+    String errorMsg = "midpoint not as expected";
+
+    int actualMidPoint;
+    actualMidPoint = positionFinder.getMidPoint(low, high);
+    assertEquals(errorMsg, expectedMidPoint, actualMidPoint);
+  }
+
+  @Test
   public void shouldReturnNegativeIntForEmptyArray() {
+    String errorMsg = "response not a negative int";
 
+    int position = positionFinder.getPosition(new int[]{}, 3);
+    assertTrue(errorMsg, position < 0);
   }
 
-  public void shouldReturnPostivePositionIfValueExistInArray() {
+  @Test
+  public void shouldReturnNegativeIntForNullArray() {
+    String errorMsg = "response not a negative int";
 
+    int position = positionFinder.getPosition(null, 3);
+    assertTrue(errorMsg, position < 0);
   }
 
+  @Test
+  public void shouldReturnPositivePositionIfValueExistInArray() {
+    String errorMsg = "position is not greater than 0";
+
+    int[] valueArray = {1, 2, 3, 4, 5};
+    int position = positionFinder.getPosition(valueArray, 3);
+    assertTrue(errorMsg, position > 0);
+  }
+
+  @Test
   public void shouldReturnNegativePositionIfValueDoesNotExistInArray() {
+    String errorMsg = "position is not less than 0";
 
+    int[] valueArray = {1, 2, 3, 4, 5};
+    int position = positionFinder.getPosition(valueArray, 9);
+    assertTrue(errorMsg, position < 0);
   }
 
+  @Test
   public void shouldReturnPositionAsExpected() {
-
+    assertPositionIsAsExpected(new int[]{0, 2, 9, 50, 800, 909, 990}, 800, 4);
+    assertPositionIsAsExpected(new int[]{1, 2, 5, 6, 7, 9, 60}, 5, 2);
+    assertPositionIsAsExpected(new int[]{1, 2, 5, 6, 7, 9, 60, 80, 90}, 90, 8);
+    assertPositionIsAsExpected(new int[]{1, 2, 5, 6, 7, 9, 60, 80, 90}, 60, 6);
+    assertPositionIsAsExpected(new int[]{1, 2, 5, 6, 7, 9, 60, 80, 90}, 2, 1);
   }
+
+  private void assertPositionIsAsExpected(int[] valueArray, int value, int expectedPosition) {
+    String errorMsg = "position not as expected";
+
+    int actualPosition = positionFinder.getPosition(valueArray, value);
+    assertEquals(errorMsg, expectedPosition, actualPosition);
+  }
+
+  //TODO: more tests can be done here for edge and special cases
 
 }
