0e5759555338ead6fbf672ccba5cca42c8f21034
moses.mansaray <m_mans@hotmail.com>
2015-09-10 23:41:36 +0100

    [DRY Fusion] - Refactor Weather and soccer feature to reduce code duplication - refactors custom exception

---
+++ data-munging/src/main/java/com/mosesmansaray/kata/data_munging/soccer/exception/SoccerLeagueDataMungingException.java
@@ -0,0 +1,18 @@
+package com.mosesmansaray.kata.data_munging.soccer.exception;
+
+import com.mosesmansaray.kata.data_munging.common.exceptions.DataMungingException;
+
+/**
+ * Created on 08/09/2015.
+ *
+ * @author moses.mansaray
+ */
+public class SoccerLeagueDataMungingException extends DataMungingException {
+
+  private static final long serialVersionUID = 8650770644582568481L;
+
+  public SoccerLeagueDataMungingException(String message) {
+    super(message);
+  }
+
+}
