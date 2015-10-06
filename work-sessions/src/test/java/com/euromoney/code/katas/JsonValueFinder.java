2d02d08d8b85579dfad82f5e47de462542cf5234
moses.mansaray <moses.mansaray@euromoneyplc.com>
2015-10-06 11:49:53 +0100

    wip - random unfinished katas

---
+++ work-sessions/src/test/java/com/euromoney/code/katas/JsonValueFinder.java
@@ -0,0 +1,14 @@
+package com.euromoney.code.katas;
+
+import org.junit.Test;
+
+/**
+ * Created by moses.mansaray on 29/09/2015.
+ */
+public class JsonValueFinder {
+
+    @Test
+    public void print(){
+        System.out.println("rrr");
+    }
+}
