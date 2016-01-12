5cf17a50fc264db0eed2195fe98cd7c2753b7a83
moses.mansaray <moses.mansaray@euromoneyplc.com>
2016-01-12 15:58:00 +0000

    WIP of santelist matchStick Day 8 of : http://adventofcode.com/day/8

---
+++ work-sessions/src/main/java/com/mosesmansaray/kata/matchsticks/SantasList.java
@@ -0,0 +1,70 @@
+package com.mosesmansaray.kata.matchsticks;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by moses.mansaray on 12/01/2016.
+ */
+public class SantasList {
+
+    String filepath;
+
+    public SantasList(String input) {
+        this.filepath = input;
+    }
+
+    public SantasList() {
+    }
+
+
+    public int getCharacterSafeInMemoryFromFile() {
+        String schedule2 = null;
+        String path = getClass().getClassLoader().getResource(filepath).getPath();
+        try {
+            byte[] allBytes = Files.readAllBytes((Paths.get(path)));
+            schedule2 = new String(allBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return getCharacterSafeInMemory(schedule2);
+    }
+
+    public int getCharacterSafeInMemory(String textToTest) {
+
+        int fileCharacterCount = getFileCharacterCount(new String(textToTest));
+
+        String characterInMem = new String(textToTest).replace("\n", "");
+        byte[] bytesCharacterInMem = DatatypeConverter.parseHexBinary(characterInMem);
+        int characterInMemCount = new String(bytesCharacterInMem, Charset.defaultCharset()).length();
+
+        int diff = characterInMemCount - fileCharacterCount;
+        return diff;
+    }
+
+    public int getFileCharacterCount(String textToTest) {
+        String[] textFile = textToTest.split("\n");
+        int count = 0;
+        for (String s : textFile) {
+            count += getCharacterCount(s);
+        }
+        return count;
+    }
+
+
+    public int getCharacterCount(String lineToTest) {
+        String replacedLine = lineToTest.replace("\\\"", "\\").replace("\\x27", "'").replace("\n", "");
+
+        replacedLine = DatatypeConverter.parseString(replacedLine);
+
+        return replacedLine.length() - 2;
+    }
+
+
+}
