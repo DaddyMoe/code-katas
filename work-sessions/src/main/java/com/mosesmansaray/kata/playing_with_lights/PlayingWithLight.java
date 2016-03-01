52fb5859c80a61b6f4e92dc946567246771cce27
moses.mansaray <m_mans@hotmail.com>
2016-03-01 23:11:34 +0000

    down from 100 seconds to 9secs

--- work-sessions/src/main/java/com/mosesmansaray/kata/playing_with_lights/PlayingWithLight.java
+++ work-sessions/src/main/java/com/mosesmansaray/kata/playing_with_lights/PlayingWithLight.java
@@ -1,28 +1,30 @@
 package com.mosesmansaray.kata.playing_with_lights;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by moses.mansaray on 01/03/2016.
  */
 public class PlayingWithLight {
-  String[] rowData;
+  String[] rows;
+  int sizeOfRows;
+  boolean[] switchGrid;
+  ;
 
-  public int[] getSwitchGrid() {
-    return switchGrid;
+  public static void main(String[] args){
+    final long startTime = System.nanoTime();
+    PlayingWithLight playingWithLight = new PlayingWithLight("lots_of_switches.txt", true);
+    playingWithLight.toggleRows();
+    System.out.println(playingWithLight.getTotalNumberOfSwitchesOn());
+    System.out.println("Took: " + TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime) );
   }
 
-  int[] switchGrid;
-
+  public boolean[] getSwitchGrid() {
+    return switchGrid;
+  }
 
   /**
    * initialise from raw string
@@ -30,10 +32,9 @@ public class PlayingWithLight {
    * @param rawData representation of the grid and child's run
    */
   public PlayingWithLight(String rawData) {
-    rowData = rawData.split("\n");
-    switchGrid = new int[Integer.parseInt(rowData[0])];
-    initializeAllSwitchesToOff(switchGrid);
-
+    rows = rawData.split("\n");
+    sizeOfRows = rows.length;
+    switchGrid = new boolean[sizeOfRows];
   }
 
 
@@ -45,35 +46,25 @@ public class PlayingWithLight {
   public PlayingWithLight(String filePath, boolean fromPath) {
     try {
       String path = getClass().getClassLoader().getResource(filePath).getPath();
-      List<String> strings = Files.readAllLines(Paths.get(path), Charset.defaultCharset());
-      rowData = new String[strings.size()];
-      rowData = strings.toArray(rowData); //TODO: List to Array!!! = this can be optimized
-      switchGrid = new int[Integer.parseInt(rowData[0])];
-      initializeAllSwitchesToOff(switchGrid);
+      byte[] encoded = Files.readAllBytes(Paths.get(path));
+      rows = new String(encoded).split("\n");
+      sizeOfRows = rows.length;
+      switchGrid = new boolean[Integer.parseInt(rows[0])];
     } catch (IOException e) {
-      // log?throw?alternative
-    }
-  }
-
-  //Alternatively use default int initialization of, taking -1 to toggle
-  private void initializeAllSwitchesToOff(int[] switchGrid) {
-    for (int i = 0; i < switchGrid.length; i++) {
-      switchGrid[i] = -1;
     }
   }
 
   public void toggleRange(int startingPos, int endPos) {
-
     if (startingPos <= endPos) {
       int i = startingPos;
       while (i <= endPos) {
-        switchGrid[i] = switchGrid[i] * -1;
+        switchGrid[i] = switchGrid[i];
         i++;
       }
-    }else {
+    } else {
       int i = startingPos;
       while (i >= endPos) {
-        switchGrid[i] = switchGrid[i] * -1;
+        switchGrid[i] = !switchGrid[i];
         i--;
       }
 
@@ -81,19 +72,26 @@ public class PlayingWithLight {
   }
 
   public void toggleRows() {
-    for (int i = 1; i < rowData.length; i++) {
-      String[] split = rowData[i].split(" ");
+//    System.out.println("start of toggle: ");
+//    startTime = System.nanoTime();
+    for (int i = 1; i < sizeOfRows; i++) {
+      String[] split = rows[i].split(" ");
       toggleRange(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
     }
+//    System.out.println("end of toggle: " + TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime) );
   }
 
   public int getTotalNumberOfSwitchesOn() {
+//    System.out.println("Start of count: ");
+//    startTime = System.nanoTime();
     int count = 0;
     for (int i = 0; i < switchGrid.length; i++) {
-        if (switchGrid[i] == 1){
+        if (switchGrid[i]){
           count ++;
         }
     }
+//    TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime);
+//    System.out.println("end of count: " + TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime));
     return count;
   }
 }
